19e8a437427537289e8786970683b5889f3b26cb
jwmitchell <jwmitchell@mbartek.com>
2020-08-31 17:15:10 -0700

    Create database and units table

--- weather_utils.py
+++ weather_utils.py
@@ -7,6 +7,10 @@ import os.path
 import json
 import sqlite3
 
+api = weather_config.config['Default']['API_ROOT']
+token = weather_config.config['Default']['API_TOKEN']
+units = weather_config.config['Default']['UNITS']
+
 def get_base_api_request_url(query_type):
     query_type_address = ''
     if query_type == 'timeseries':
@@ -17,24 +21,52 @@ def get_base_api_request_url(query_type):
     base_api_request_url = os.path.join(api, "stations/timeseries")
     return base_api_request_url
 
-api = weather_config.config['Default']['API_ROOT']
-token = weather_config.config['Default']['API_TOKEN']
-units = weather_config.config['Default']['UNITS']
+def create_weather_db(db_path_name):
+    if db_path_name == '':
+        db_path_name = 'example.db'
+    connection = None
+    try:
+        connection = sqlite3.connect(db_path_name)
+    except Error as e:
+        print(e)
 
-radius = (38.09,-122.65,10)
-st_radius = ",".join(map(str,radius))
+    c = connection.cursor()
+    # Get example dataset
+    radius_data = get_example_radius_dataset()
+    print("Creating tables for " + db_path_name)
 
-api_arguments = {"token":token,"start":"201910092300","end":"201910101700","radius":st_radius,"units":units}
+    # Create table for UNITS
+    c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='units' ''')
+    if c.fetchone()[0] !=1:        #Skip if table exists
+        c.execute('''CREATE TABLE units (
+        variable text NOT NULL,
+        units text NOT NULL
+        );''')
+        sql = 'INSERT INTO units(variable,units) VALUES(?,?)'
+        values =[]
+        for v,u in radius_data['UNITS'].items():
+            values.append((v,u))
+        print("loading values")
+        c.executemany(sql,values)
 
-api_request_url = get_base_api_request_url("timeseries")
+        
 
-req = requests.get(api_request_url, params=api_arguments)
+def get_example_radius_dataset():
+    radius = (38.09,-122.65,3)
+    st_radius = ",".join(map(str,radius))
 
-# response = req.urlopen(api_request_url, api_arguments)
+    api_arguments = {"token":token,"start":"201910092300","end":"201910101700","radius":st_radius,"units":units}
+    api_request_url = get_base_api_request_url("timeseries")
+    req = requests.get(api_request_url, params=api_arguments)
+    data = req.json()
+    return(data)
 
-data = req.json()
+# radius_data = get_example_radius_dataset()
 
-print(data)
+create_weather_db("test_example.db")
+print("finished")
+
+
 
 def date_to_int(date_string):
     print ("date_string")
