03fcc52f7afab8ebc424392cdb24c913beacbfe7
jwmitchell <jwmitchell@mbartek.com>
2020-09-26 07:39:51 -0700

    Moved creation into the WeatherDB class

--- weather_utils.py
+++ weather_utils.py
@@ -22,95 +22,6 @@ def get_base_api_request_url(query_type):
     base_api_request_url = os.path.join(api, "stations/timeseries")
     return base_api_request_url
 
-def create_weather_db(db_path_name):
-    if db_path_name == '':
-        db_path_name = 'example.db'
-    connection = None
-    try:
-        connection = sqlite3.connect(db_path_name)
-    except Error as e:
-        print(e)
-
-    cc = connection.cursor()
-    # Get example dataset
-    radius_data = get_example_radius_dataset()
-    print("Creating tables for " + db_path_name)
-
-    # Create table for UNITS
-    cc.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='units' ''')
-    if cc.fetchone()[0] ==1:        #Error if table exists
-        raise RuntimeError("SQL table units already exists")
-    cc.execute('''CREATE TABLE units (
-    variable text NOT NULL,
-    units text NOT NULL
-    );''')
-    sql = 'INSERT INTO units(variable,units) VALUES(?,?)'
-    values =[]
-    for vv,uu in radius_data['UNITS'].items():
-        values.append((vv,uu))
-    cc.executemany(sql,values)
-
-    # Create station table
-    cc.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='station' ''')
-    if cc.fetchone()[0] ==1:        #Error if table exists
-        raise RuntimeError("SQL table station already exists")
-    sql = 'CREATE TABLE station (sid INTEGER PRIMARY KEY, '
-    iv = 0
-    vlen = len(radius_data['STATION'][0].keys())
-    for v in radius_data['STATION'][0].keys():
-        iv +=1
-        sqltype = python_to_sql(radius_data['STATION'][0][v])
-        print("Station var: " + v + "   SQL Type: " + sqltype)
-        comma = ', '
-        if sqltype != 'REFERENCE' and v not in ['OBSERVATIONS']:
-            sql = sql + v.lower() + '  ' + sqltype
-        else:
-            if v == 'OBSERVATIONS':    #OBSERVATIONS is foreign table with STID as a foreign key
-                comma = ''
-            elif v == 'SENSOR_VARIABLES':
-                sql = sql + v.lower() + ' BLOB  NOT NULL'
-            elif v == 'PERIOD_OF_RECORD':
-                por_start =  radius_data['STATION'][0]['PERIOD_OF_RECORD']['start']
-                por_end =  radius_data['STATION'][0]['PERIOD_OF_RECORD']['end']
-                sql = sql + 'period_of_record_start TEXT  NOT NULL, '
-                sql = sql + 'period_of_record_stop TEXT  NOT NULL'
-            else:
-                raise ValueError("Unknown station reference variable " + v)
-
-        if iv != vlen:
-            sql = sql + comma
-        
-    sql = sql + ', UNIQUE(stid));'
-    print(sql)
-    try:
-        cc.execute(sql)
-    except Error as e:
-        print(e)
-
-    # Create observations table
-
-    cc.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='observations' ''')
-    if cc.fetchone()[0] ==1:        #Error if table exists
-        raise RuntimeError("SQL table observations already exists")
-    sql = 'CREATE TABLE observations (date_time TEXT PRIMARY KEY, '
-    iv = 0
-    vlen = len(radius_data['STATION'][0]['OBSERVATIONS'].keys())
-    for v in radius_data['STATION'][0]['OBSERVATIONS'].keys():
-        iv +=1
-        sqltype = python_to_sql(radius_data['STATION'][0]['OBSERVATIONS'][v][0])
-        print("Observation var: " + v + "   SQL Type: " + sqltype)
-        comma = ', '
-        if sqltype != 'REFERENCE' and v != 'date_time':
-            sql = sql + v.lower() + '  ' + sqltype + comma
-        
-    sql = sql + ' stid TEXT NOT NULL, FOREIGN KEY (stid) REFERENCES station (stid) );'
-    print(sql)
-    try:
-        cc.execute(sql)
-    except Error as e:
-        print(e)
-
-    connection.commit()
 
 def python_to_sql(obj):
     sqltype = 'NULL'
@@ -227,15 +138,25 @@ def get_station_by_stid(stid,db_name):
 
 class WeatherDB(object):
 
+    # Class WeatherDB handles all operations on the weather data caching database. It makes
+    # persistent connections available without requiring open and close operations.
+    # Because sqlite will create a file if passed a filename that does not exist, there is
+    # the potential for accidentally creating a database when one was to be opened. To prevent
+    # this contingency, the creation of a WeatherDB object must be performed via the static
+    # method WeatherDB.create(newdbfilename), which returns a WeatherDB object. Binding a
+    # WeatherDB object to an existing database simply uses the constructor
+    # WeatherDB(existingdbfilename).
+
     def __init__(self,db_name):
         if not os.path.isfile(db_name):
             raise FileExistsError(db_name + " does not exist, use WeatherDB.create(db_name)")
-        print("Opening " + db_name)
 
+        print("Opening " + db_name)
         try:
             connection = sqlite3.connect(db_name)
         except Error as e:
             print(e)
+
         self.connection = connection
         self.cursor = connection.cursor()
         self.db_name = db_name
@@ -244,9 +165,94 @@ class WeatherDB(object):
         if os.path.isfile(db_name):
             raise ValueError(db_name + " already exists. Use WeatherDB(db_name).")
         print("Creating " + db_name)
-        create_weather_db(db_name)
+        db_file = open(db_name,'w')
+        db_file.close()
         mydb = WeatherDB(db_name)
+        try:
+            mydb.connection = sqlite3.connect(db_name)
+        except Error as e:
+            print(e)
+        mydb.cursor = mydb.connection.cursor()
+        
+        # Get example dataset - Currently this is dynamically pulled from Synoptic, probably
+        # should be static data
+        radius_data = get_example_radius_dataset()
+        print("Creating tables for " + db_name)
+
+        # Create table for UNITS
+        mydb.cursor.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='units' ''')
+        if mydb.cursor.fetchone()[0] ==1:        #Error if table exists
+            raise RuntimeError("SQL table units already exists")
+        sql = "CREATE TABLE units (variable text NOT NULL,units text NOT NULL);"
+        mydb.cursor.execute(sql)
+        sql = 'INSERT INTO units(variable,units) VALUES(?,?)'
+        values =[]
+        for vv,uu in radius_data['UNITS'].items():
+            values.append((vv,uu))
+            mydb.cursor.executemany(sql,values)
+
+        # Create station table
+        mydb.cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='station'")
+        if mydb.cursor.fetchone()[0] ==1:        #Error if table exists
+            raise RuntimeError("SQL table station already exists")
+        sql = "CREATE TABLE station (sid INTEGER PRIMARY KEY, "
+        iv = 0
+        vlen = len(radius_data['STATION'][0].keys())
+        for v in radius_data['STATION'][0].keys():
+            iv +=1
+            sqltype = python_to_sql(radius_data['STATION'][0][v])
+            print("Station var: " + v + "   SQL Type: " + sqltype)
+            comma = ', '
+            if sqltype != 'REFERENCE' and v not in ['OBSERVATIONS']:
+                sql = sql + v.lower() + '  ' + sqltype
+            else:
+                if v == 'OBSERVATIONS':    #OBSERVATIONS is foreign table with STID as a foreign key
+                    comma = ''
+                elif v == 'SENSOR_VARIABLES':
+                    sql = sql + v.lower() + ' BLOB  NOT NULL'
+                elif v == 'PERIOD_OF_RECORD':
+                    por_start =  radius_data['STATION'][0]['PERIOD_OF_RECORD']['start']
+                    por_end =  radius_data['STATION'][0]['PERIOD_OF_RECORD']['end']
+                    sql = sql + 'period_of_record_start TEXT  NOT NULL, '
+                    sql = sql + 'period_of_record_stop TEXT  NOT NULL'
+                else:
+                    raise ValueError("Unknown station reference variable " + v)     
+
+            if iv != vlen:
+                sql = sql + comma
+        
+        sql = sql + ', UNIQUE(stid));'
+        print(sql)
+        try:
+            mydb.cursor.execute(sql)
+        except Error as e:
+            print(e)
+
+        # Create observations table
+        mydb.cursor.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='observations' ''')
+        if mydb.cursor.fetchone()[0] ==1:        #Error if table exists
+            raise RuntimeError("SQL table observations already exists")
+        sql = 'CREATE TABLE observations (date_time TEXT PRIMARY KEY, '
+        iv = 0
+        vlen = len(radius_data['STATION'][0]['OBSERVATIONS'].keys())
+        for v in radius_data['STATION'][0]['OBSERVATIONS'].keys():
+            iv +=1
+            sqltype = python_to_sql(radius_data['STATION'][0]['OBSERVATIONS'][v][0])
+            print("Observation var: " + v + "   SQL Type: " + sqltype)
+            comma = ', '
+            if sqltype != 'REFERENCE' and v != 'date_time':
+                sql = sql + v.lower() + '  ' + sqltype + comma
+        
+        sql = sql + ' stid TEXT NOT NULL, FOREIGN KEY (stid) REFERENCES station (stid) );'
+        print(sql)
+        try:
+            mydb.cursor.execute(sql)
+        except Error as e:
+            print(e)
+
+        mydb.connection.commit()
         return mydb
+
     create = staticmethod(create)
 
     def close(self):
